5fe16bfbc7deafcbad6722b71391b4b710fd087e
Valentin Doche <contact@valentindoche.com>
2019-09-30 13:07:06 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <BME280I2C.h>
+
+SoftwareSerial hc12(2,3);
+
+boolean start;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  hc12.begin(9600);  
+
+  Serial.println("Hello World");
+
+  hc12.write("AT");
+  delay(50);
+  int return1 = hc12.read();
+  int return2 = hc12.read();
+
+  if (return1 == 79 && return2 == 75)
+  {
+    start = true;
+  }
+  
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly: 
+  if (start == 1)
+  {
+    /* code */  
+  }
+  
+
 }
